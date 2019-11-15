234c0804c52bff2cfb951d306c8c33604fed3ffc
Carlos Fabio <cfabioramos@gmail.com>
2019-11-15 08:13:27 -0200

    First update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  // Declare a new state variable, which we'll call "count"
+  // This new function useState is a “Hook”
+  // React 16.8.0 is the first release to support Hooks.
+  const [count, setCount] = useState(0);
+  const [name, setName] = useState("Carlos Fábio");
+  // The Effect Hook, useEffect, adds the ability to perform side effects.
+  // It serves the same purpose as componentDidMount, componentDidUpdate, and componentWillUnmount 
+  //  in React classes, but unified into a single API.
+  // In this example, this component sets the document title after React updates the DOM:
+  // Similar to componentDidMount and componentDidUpdate:
+  useEffect(() => {
+    // Update the document title using the browser API
+    document.title = `You clicked ${count} times`;
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>React Studies</h2>
+      <p><button onClick={() => {
+        setCount(count + 1)
+        setName("João")}} >Click me</button></p>
+      <div>{name}, You clicked {count} times</div>
     </div>
   );
+
 }
 
 export default App;
